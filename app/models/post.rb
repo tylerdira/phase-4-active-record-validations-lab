fdc093898904763ac4d377a1bf75f6c2f0014ebd
Joseph Tyler DiRaffaele <tylerdiraffaele@gmail.com>
2022-10-13 16:08:16 -0700

    commit tree df2716eea29f2bda4e48b5f56a545795129ff688

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+
+    validate :clickbate
+
+    @@titles = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+
+    def clickbate
+        if @@titles.none? {|char| char.match(title)}
+            errors.add(:title, "its not clickbait")
+        end
+    end
 end
